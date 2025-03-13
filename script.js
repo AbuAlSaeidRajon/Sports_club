cb5dbfb99d5c134a973bc92440f165f81a221462
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-03-13 14:36:27 +0200

    style changed made

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const animatedText = document.getElementById("animatedText");
-    animatedText.textContent = "কোমরপুর বিজয় নিশান ক্লাব";
+document.addEventListener('DOMContentLoaded', function() {
+    const animatedText = document.getElementById('animatedText');
+    animatedText.textContent = 'কোমরপুর বিজয় নিশান ক্লাব';
 });
